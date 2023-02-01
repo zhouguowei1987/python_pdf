db88493c968137cbe1fc9c3403290c7659b17981
729044963@qq.com <729044963@qq.com>
2023-02-01 17:06:57 +0800

    测试

--- main.py
+++ main.py
@@ -2,94 +2,92 @@
 # @Time   : 2022-02-23
 # @Author : carl_DJ
 
-from PyPDF4 import PdfFileReader, PdfFileWriter
-from PyPDF4.pdf import ContentStream
-from PyPDF4.generic import TextStringObject, NameObject
-from PyPDF4.utils import b_
+from PyPDF2 import PdfReader, PdfWriter
 import os
+
+
+def replace_text(content, replacements=None):
+    if replacements is None:
+        replacements = dict()
+    lines = content.splitlines()
+
+    result = ""
+    in_text = False
+
+    for line in lines:
+        if line == "BT":
+            in_text = True
+
+        elif line == "ET":
+            in_text = False
+
+        elif in_text:
+            cmd = line[-2:]
+            if cmd.lower() == 'tj':
+                print(line)
+                replaced_line = line
+                for k, v in replacements.items():
+                    replaced_line = replaced_line.replace(k, v)
+                result += replaced_line + "\n"
+            else:
+                result += line + "\n"
+            continue
+
+        result += line + "\n"
+
+    return result
+
+
+def process_data(stream_obj, replacements):
+    data = stream_obj.get_data()
+    decoded_data = data.decode('latin1')
+
+    replaced_data = replace_text(decoded_data, replacements)
+
+    encoded_data = replaced_data.encode('latin1')
+    if stream_obj.decoded_self is not None:
+        stream_obj.decoded_self.set_data(encoded_data)
+    else:
+        stream_obj.set_data(encoded_data)
 
 
 # 去除pdf的水印
 def remove_pdf_watermark():
-    s = b'\x07\xfc'
-
-    print(type(s))
-    print(s)
-
-    ss = str(s, 'utf8')
-    print(ss)
-
-    exit(1)
-
     pdf_dir = "../aaa/"
+    replacements = {"全国团体标准信息平台": "你好啊"}
     files = sorted(os.listdir(pdf_dir))
     for file in files:
         if ".pdf" in file:
             print(file)
             try:
-                output = PdfFileWriter()
+                output = PdfWriter()
                 pdffile = pdf_dir + file
-                source = PdfFileReader(pdffile, 'rb')
+                source = PdfReader(pdffile, 'rb')
 
-                if source.getNumPages() < 3:
+                if len(source.pages) < 3:
                     print("删除文件")
                     os.remove(pdffile)
                     continue
 
-                page = source.getPage(0)
-                if len(page.extractText()) <= 0:
+                if len(source.pages[0].extract_text()) <= 0:
                     print("删除文件")
                     os.remove(pdffile)
                     continue
 
-                # page0 = source.getPage(0)
-                # page1 = source.getPage(1)
-                # page2 = source.getPage(2)
-                # print(page0)
-                # print("=====")
-                # print(page1)
-                # print("=====")
-                # print(page2)
-                # print("=====")
-                # exit(1)
-
-                # page = source.getPage(1)
-                # content_object = page["/Contents"].getObject()
-                # content = ContentStream(content_object, source)
-                # print(content.operations)
-                # exit(1)
-
-                for i in range(source.getNumPages()):
-                    page = source.getPage(i)
-
-                    # if '/XObject' in page['/Resources'].keys():
-                    #     page['/Resources']['/XObject'].clear()
-
-                    # if '/Font' in page['/Resources'].keys():
-                    #     if '/Xi' + str(i * 2 + 1) in page['/Resources']['/Font'].keys():
-                    #         page['/Resources']['/Font']['/Xi' + str(i * 2 + 1)].clear()
-                    #
-                    #     if '/Xi' + str(i * 2 + 3) in page['/Resources']['/Font'].keys():
-                    #         page['/Resources']['/Font']['/Xi' + str(i * 2 + 3)].clear()
-
-                    # if '/ExtGState' in page['/Resources'].keys():
-                    #     page['/Resources']['/ExtGState'].clear()
-                    # if '/Font' in page['/Resources'].keys():
-                    #     page['/Contents'].clear()
-
-                    content_object = page["/Contents"].getObject()
-                    content = ContentStream(content_object, source)
-
-                    for operands, operator in content.operations:
-                        if operator == b_("Tj"):
-                            text = operands[0]
-                            print(text)
-                            if isinstance(text, str) and text.startswith('全国团体标准信息平台'):
-                                operands[0] = TextStringObject()
-
-                    page.__setitem__(NameObject('/Contents'), content)
+                for page in source.pages:
+                    contents = page.get_contents()
+                    if len(contents) > 0:
+                        for obj in contents:
+                            stream_obj = obj.get_object()
+                            try:
+                                process_data(stream_obj, replacements)
+                            except Exception as e:
+                                print(e)
+                                print("shit when wrong")
+                    else:
+                        process_data(contents, replacements)
+                    output.add_page(page)
                     exit(1)
-                    output.addPage(page)
                 with open(pdffile, 'wb') as ouf:
                     output.write(ouf)
             except Exception as e:
