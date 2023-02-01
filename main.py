0d1e84ddadedaa37b10fa7e480d59f742b6df6ab
周国伟 <zhouguowei@zhouguoweideMacBook-Pro.local>
2023-02-02 00:25:06 +0800

    测试

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # @Author : carl_DJ
 
 from PyPDF2 import PdfReader, PdfWriter
+from PyPDF2.generic import ByteStringObject, TextStringObject
+import re
 import os
 
 
@@ -24,7 +26,6 @@ def replace_text(content, replacements=None):
         elif in_text:
             cmd = line[-2:]
             if cmd.lower() == 'tj':
-                print(line)
                 replaced_line = line
                 for k, v in replacements.items():
                     replaced_line = replaced_line.replace(k, v)
@@ -39,12 +40,13 @@ def replace_text(content, replacements=None):
 
 
 def process_data(stream_obj, replacements):
+    print(type(stream_obj))
     data = stream_obj.get_data()
-    decoded_data = data.decode('latin1')
+    decoded_data = data.decode('utf-8')
 
     replaced_data = replace_text(decoded_data, replacements)
 
-    encoded_data = replaced_data.encode('latin1')
+    encoded_data = replaced_data.encode('utf-8')
     if stream_obj.decoded_self is not None:
         stream_obj.decoded_self.set_data(encoded_data)
     else:
@@ -54,7 +56,7 @@ def process_data(stream_obj, replacements):
 # 去除pdf的水印
 def remove_pdf_watermark():
     pdf_dir = "../aaa/"
-    replacements = {"全国团体标准信息平台": "你好啊"}
+    replacements = {"Quantifying": "111"}
     files = sorted(os.listdir(pdf_dir))
     for file in files:
         if ".pdf" in file:
@@ -64,10 +66,10 @@ def remove_pdf_watermark():
                 pdffile = pdf_dir + file
                 source = PdfReader(pdffile, 'rb')
 
-                if len(source.pages) < 3:
-                    print("删除文件")
-                    os.remove(pdffile)
-                    continue
+                # if len(source.pages) < 3:
+                #     print("删除文件")
+                #     os.remove(pdffile)
+                #     continue
 
                 if len(source.pages[0].extract_text()) <= 0:
                     print("删除文件")
@@ -76,24 +78,52 @@ def remove_pdf_watermark():
 
                 for page in source.pages:
                     contents = page.get_contents()
+                    # bdata = contents.get_data()
+                    # print(11)
+                    # ddata = bdata.decode('latin1')  # decoded data (string)
+                    # print(ddata)
+                    # print(22)
+                    # for key in replacements.keys():
+                    #     ddata = ddata.replace(key, replacements[key])
+                    # print(ddata)
+                    # print(333)
+                    # contents.set_data(ddata.encode('latin1'))
+                    # exit(1)
+                    ss = '全国团体标准信息平台'
+
                     if len(contents) > 0:
                         for obj in contents:
-                            stream_obj = obj.get_object()
-                            try:
-                                process_data(stream_obj, replacements)
-                            except Exception as e:
-                                print(e)
-                                print("shit when wrong")
+                            ddata = obj.get_object().get_data()
+                            print(TextStringObject(ss.encode(), 'utf-8').get_object())
+                            print(ddata)
+                            print("====================")
+                            # try:
+                            #     process_data(stream_obj, replacements)
+                            # except Exception as e:
+                            #     print(e)
+                            #     print("shit when wrong")
                     else:
                         process_data(contents, replacements)
-                    output.add_page(page)
                     exit(1)
+                    # if len(contents) > 0:
+                    #     for obj in contents:
+                    #         stream_obj = obj.get_object()
+                    #         print(stream_obj)
+                    #         exit(1)
+                    #         try:
+                    #             process_data(stream_obj, replacements)
+                    #         except Exception as e:
+                    #             print(e)
+                    #             print("shit when wrong")
+                    # else:
+                    #     process_data(contents, replacements)
+                    output.add_page(page)
                 with open(pdffile, 'wb') as ouf:
                     output.write(ouf)
             except Exception as e:
                 print(e)
-                print("删除文件")
-                os.remove(pdffile)
+                # print("删除文件")
+                # os.remove(pdffile)
 
 
 if __name__ == '__main__':
