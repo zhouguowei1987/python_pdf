1e8d97640e69c53af6b61c205b1bba40f56ed32f
周国伟 <zhouguowei@zhouguoweideMacBook-Pro.local>
2023-06-30 11:24:11 +0800

    冲突

--- 51test.py
+++ 51test.py
@@ -3,6 +3,8 @@ from docx2pdf import convert
 if __name__ == '__main__':
     word_file = "../upload.doc88.com/finish.tikuvip（2023）.51test.net-高考/2023年江苏苏州中考道德与法治真题(含答案).docx"
     pdf_file = word_file.replace(".docx", ".pdf")
+    file = open(pdf_file, "w")
+    file.close()
     # 将 Word 文档转换为 PDF
     try:
         convert(word_file, pdf_file)
