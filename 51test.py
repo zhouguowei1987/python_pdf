18a462a4c1a80be871f883403733f2fe6daf9149
729044963@qq.com <729044963@qq.com>
2023-06-30 11:22:42 +0800

    word转pdf

--- 51test.py
+++ 51test.py
@@ -1,11 +1,12 @@
 from docx2pdf import convert
 
 if __name__ == '__main__':
-    word_file = "../upload.doc88.com/finish.tikuvip（2023）.51test.net-高考/2023年江苏苏州中考道德与法治真题(含答案).docx"
+    word_file = "2023年安徽亳州中考历史试题及答案.docx"
     pdf_file = word_file.replace(".docx", ".pdf")
-    # 将 Word 文档转换为 PDF
-    try:
-        convert(word_file, pdf_file)
-        print("转换成功！")
-    except Exception as e:
-        print("转换失败：", str(e))
+    with open(pdf_file, "w") as f:
+        # 将 Word 文档转换为 PDF
+        try:
+            convert(word_file, pdf_file)
+            print("转换成功！")
+        except Exception as e:
+            print("转换失败：", str(e))
