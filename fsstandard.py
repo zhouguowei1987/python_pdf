d4e87ff81cffe41d4385c8c0aa098b83bb95f26d
zhouguowei <729044963@qq.com>
2026-07-22 09:01:05 +0800

    提交

---
+++ fsstandard.py
@@ -0,0 +1,67 @@
+# -*- coding:utf-8 -*-
+# @Time   : 2022-02-23
+# @Author : carl_DJ
+import json
+
+import fitz
+import os
+
+
+# 去除pdf的水印
+def remove_pdf_watermark():
+    pdf_dir = "../temp-www.fsstandard.org.cn/"
+    files = sorted(os.listdir(pdf_dir))
+    for file in files:
+        is_save_new_file = True
+        if ".pdf" in file:
+            print(file)
+            pdf_file = pdf_dir + file
+            try:
+                doc = fitz.open(pdf_file)
+                if len(doc[0].get_text('dict')) <= 0:
+                    print("删除文件111")
+                    os.remove(pdf_file)
+                    continue
+                pdf_new_file = '../upload.doc88.com/hbba.sacinfo.org.cn/' + file.replace("佛山标准-", "")
+
+                # 记录需要删除页面id
+                delete_page_ids = []
+                for pno in range(doc.page_count):
+                    page = doc[pno]
+
+                    page.clean_contents()
+                    xref = page.get_contents()[0]
+                    cont = bytearray(page.read_contents())
+                    # if pno == 2:
+                    #     print(cont)
+                    #     exit()
+                    # 删除佛山地方标准水印
+                    im1 = cont.rfind(b'/FXX1 Do Q EMC Q')
+                    if im1 >= 0:
+                        im2 = cont.rfind(b"/Artifact<</Type/Pagination/Subtype/Watermark>>", 0, im1)
+                        if im2 >= 0:
+                            cont[im2: im1 + 16] = b""
+                    doc.update_stream(xref, cont)
+                if os.path.exists(pdf_new_file):
+                    os.remove(pdf_new_file)
+
+                if len(delete_page_ids):
+                    doc.delete_pages(delete_page_ids)
+
+                if doc.page_count < 5:
+                    print("删除文件222")
+                    os.remove(pdf_file)
+                    os.remove(pdf_new_file)
+                    continue
+                if is_save_new_file:
+                    doc.save(pdf_new_file)
+                doc.close()
+                print("删除源文件")
+                os.remove(pdf_file)
+            except Exception as e:
+                print(e)
+                print("删除文件333")
+
+
+if __name__ == '__main__':
+    remove_pdf_watermark()
